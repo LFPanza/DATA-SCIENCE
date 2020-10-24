bc9f6775d7767f9b433425ab249114f8d6fdfbc4
Luiz Fernando Panza <luizfernandopanza@yahoo.com.br>
2020-10-24 19:42:23 -0300

    Update and rename 03-Estudo_Caso.R to Manipulando Dados de Voos.R

--- Manipulando Dados de Voos.R
+++ Manipulando Dados de Voos.R
@@ -1,14 +1,4 @@
-# Estudo de Caso - Limpando, Transformando e Manipulando Dados de Voos
-
-# Obs: Caso tenha problemas com a acentuação, consulte este link:
-# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
-
-# Configurando o diretório de trabalho
-# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-# Não use diretórios com espaço no nome
-setwd("C:/FCD/BigDataRAzure/Cap07")
-getwd()
-
+# Limpando, Transformando e Manipulando Dados de Voos
 
 # Instalando pacote hflights (Dados de voos de Houston)
 install.packages("hflights")
