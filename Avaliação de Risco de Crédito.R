cebb9256a8f9566a1e7855189ba06664a4d613c4
Luiz Fernando Panza <luizfernandopanza@yahoo.com.br>
2020-10-24 21:26:51 -0300

    Update and rename AvaliaRiscoCredito.R to Avaliação de Risco de Crédito.R

--- Avaliação de Risco de Crédito.R
+++ Avaliação de Risco de Crédito.R
@@ -1,13 +1,5 @@
 # Construindo um Modelo Preditivo para Análise de Risco
 
-# Obs: Caso tenha problemas com a acentuação, consulte este link:
-# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
-
-# Configurando o diretório de trabalho
-# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-setwd("~/Dropbox/DSA/BigDataAnalytics-R-Azure/Projetos/Mini-Projeto04")
-getwd()
-
 # Carregando o dataset em um dataframe
 credit.df <- read.csv("credit_dataset.csv", header = TRUE, sep = ",")
 head(credit.df)
