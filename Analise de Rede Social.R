5e9832d0f6eecf7273845e3778031e6df30d2c7a
Luiz Fernando Panza <luizfernandopanza@yahoo.com.br>
2020-10-24 21:25:44 -0300

    Update and rename AnaliseRedeSocial-v2.R to Analise de Rede Social.R

--- Analise de Rede Social.R
+++ Analise de Rede Social.R
@@ -1,12 +1,4 @@
-# Mini-Projeto 1 - Análise de Sentimentos em Redes Sociais
-
-# Obs: Caso tenha problemas com a acentuação, consulte este link:
-# https://support.rstudio.com/hc/en-us/articles/200532197-Character-Encoding
-
-# Configurando o diretório de trabalho
-# Coloque entre aspas o diretório de trabalho que você está usando no seu computador
-setwd("~/Dropbox/DSA/BigDataAnalytics-R-Azure/Projetos/Mini-Projeto01")
-getwd()
+# Análise de Sentimentos em Redes Sociais
 
 ## Etapa 1 - Pacotes e Autenticação
 
